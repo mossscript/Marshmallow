7a799a37178fea028f18a2384ca6d39ebade04b2
leafweb <leafweb.io@gmail.com>
2024-08-18 17:58:07 +0330

    update

--- Marshmallow/Marshmallow.js
+++ Marshmallow/Marshmallow.js
@@ -15,19 +15,22 @@ let MARSHMALLOW = () => {
       storage[x] = y;
       localStorage.setItem('Marshmallow', JSON.stringify(storage));
    }
-   let defaultData = {
-      darkmode: 'auto',
-      blackUI: false,
-      fontSize: 14,
-      radius: 8,
-      freeze: true
-   }
-   let storage = getStorage();
-   if (!storage) saveStorage(defaultData);
    let ApplySettings = () => {
       let root = document.querySelector(':root');
       root.style.setProperty('--m-font-size', storage.fontSize);
       root.style.setProperty('--m-radius', storage.radius);
+   }
+   let defaultData = {
+      darkmode: 'auto',
+      blackUI: false,
+      fontSize: '14px',
+      radius: '8px',
+      freeze: true
+   }
+   let storage = getStorage();
+   if (!storage) {
+      storage = defaultData;
+      saveStorage(defaultData);
    }
    ApplySettings();
    // Mushroom 
@@ -118,41 +121,54 @@ let MARSHMALLOW = () => {
    }
 };
 let Marshmallow = MARSHMALLOW();
-Marshmallow.setColorScheme('square');
 
 // Components 
 class MBtn extends HTMLElement {
-   constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-   }
-   static get observedAttributes() {
-      return ['color','size','shadow','badge','icon','disabled'];
-   }
-   attributeChangedCallback(name, oldValue, newValue) {
-      this.render();
-   }
-   connectedCallback() {
-      this.render();
-   }
-   render() {
-      this.shadowRoot.innerHTML = `
-         <button part="button" ${this.hasAttribute('disabled')?'disabled':''}>
-            ${this.getAttribute('badge') ? `<span part="badge">${this.getAttribute('badge')}</span>`:''}
-            ${this.getAttribute('icon') ? `<span part="icon">${this.getAttribute('icon')}</span>`:''}
-            <slot part="text"></slot>
-         </button>
-      `;
-    
-      let button = this.shadowRoot.querySelector('button');
-      let arr = ['color','size','shadow','badge','icon'];
-      Array.from(this.attributes).forEach(attr => {
-         if (!arr.includes(attr.name)) {
-            button.setAttribute(attr.nane, attr.value);
-         }
-      });
-   }
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  static get observedAttributes() {
+    return ['color', 'badge', 'badge-color', 'badge-positon', 'icon-align', 'disabled'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+
+  connectedCallback() {
+    this.render();
+    this.addSlotListeners();
+  }
+
+  addSlotListeners() {
+    let badgeSlot = this.shadowRoot.querySelector('slot[name="badge"]');
+    let iconSlot = this.shadowRoot.querySelector('slot[name="icon"]');
+    this.shadowRoot.addEventListener('slotchange', (event) => {
+       if (badgeSlot && !badgeSlot.assignedNodes().length) badgeSlot.parentElement.remove();
+       if (iconSlot || !iconSlot.assignedNodes().length) iconSlot.parentElement.remove();
+    });
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <button part="button" ${this.hasAttribute('disabled') ? 'disabled' : ''}>
+        ${this.getAttribute('badge') ? `<span part="badge">${this.getAttribute('badge')}</span>`:'<span part="badge"><slot name="badge"></slot></span>'}
+        <span part="icon"><slot name="icon"></slot></span>
+        <span part="text"><slot></slot></span>
+      </button>
+    `;
+    let button = this.shadowRoot.querySelector('button');
+    let arr = ['color', 'badge', 'badge-color', 'badge-positon', 'icon-align'];
+    Array.from(this.attributes).forEach(attr => {
+      if (!arr.includes(attr.name)) {
+        button.setAttribute(attr.name, attr.value);
+      }
+    });
+  }
 }
+
 customElements.define('m-btn', MBtn);
 
 // Add-On (...)
