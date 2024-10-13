e3e818ee93961308b3c5c4a7da08605a8e89e065
leafweb <leafweb.io@gmail.com>
2024-10-13 23:42:55 +0330

    update

--- Marshmallow/components/m-button.js
+++ Marshmallow/components/m-button.js
@@ -9,15 +9,13 @@ export default class MButton extends HTMLElement {
       this.shadow = this.attachShadow({ mode: 'open' });
       this.background = undefined;
       this.color = undefined;
-      this.width = undefined;
-      this.height = undefined;
+      this.active = false;
    }
    static get observedAttributes() {
       return ['color'];
    }
    connectedCallback() {
       this.#render();
-      this.#observeResize();
       this.#setColor(this.color);
    }
    attributeChangedCallback(name, oldValue, newValue) {
@@ -29,14 +27,20 @@ export default class MButton extends HTMLElement {
    #render() {
       this.shadow.innerHTML = `
             <style>
-               :host {
-                  position: relative;
-                  display: inline-block;
+               :host{
+                  display: inline-flex;
                   cursor: pointer;
                   box-sizing: border-box;
-                  webkit-tap-highlight-color: transparent;
-                  width:${this.width}px;
-                  height:${this.height}px;
+                  -webkit-tap-highlight-color: transparent;
+               }
+               :host::part(btn) {
+                  position: relative;
+                  display: block;
+                  overflow: hidden;
+                  color:${this.color?this.color:'var(--m-on-primary)'};
+                  border-radius: calc(var(--m-radius) + 8px);
+                  font-weight: 600;
+                  transition: 0.3s;
                }
                :host::part(bg) {
                   position: absolute;
@@ -47,28 +51,26 @@ export default class MButton extends HTMLElement {
                   background: ${this.background?this.background:'var(--m-primary)'};
                   width: 100%;
                   height: 100%;
+                  transition: 0.3s;
                }
                :host::part(inner) {
                   position: relative;
                   z-index: 1;
                   display: block;
-                  padding: 10px;
-                  color:${this.color?this.color:'var(--m-on-primary)'};
+                  padding: 12px 24px;
+                  font-size: calc(var(--m-font-size)+0px);
+                  user-select: none;
+               }
+               :host::part(btn):active{
+                  scale: 0.9;
+                  border-radius: calc(var(--m-radius) + 4px);
                }
             </style>
-            <div part="bg"></div>
-            <div part="inner"><slot></slot></div>
+            <div part="btn">
+               <div part="bg"></div>
+               <slot part="inner"></slot>
+            </div>
         `;
-   }
-   #observeResize() {
-      let bg = this.shadow.querySelector('[part="bg"]');
-      let inner = this.shadow.querySelector('[part="inner"]');
-      let resizeObserver = new ResizeObserver(() => {
-         this.width = inner.offsetWidth;
-         this.height = inner.offsetHeight;
-      });
-      resizeObserver.observe(inner);
-      this.#render();
    }
    #setColor(param) {
       let host = this.shadow.host;
@@ -77,7 +79,7 @@ export default class MButton extends HTMLElement {
          let testColor = TC.test(param);
          let color = param;
          if (/^@/.test(param)) {
-            let val = getComputedStyle(host).getPropertyValue(color.replace('@', '--m-'));
+            let val = getComputedStyle(host).getPropertyValue(color.toLowerCase().replace('@', '--m-'));
             color = val ? val : color;
             testColor = !!val;
          } else if (/^var\((--[a-zA-Z0-9-]+)\)$/.test(param)) {
@@ -88,7 +90,7 @@ export default class MButton extends HTMLElement {
          this.background = testColor ? color : 'var(--m-primary)';
          let innerColor = TC.inner(color);
          if (/^@/.test(param)) {
-            let val = getComputedStyle(host).getPropertyValue(param.replace('@', '--m-on-'));
+            let val = getComputedStyle(host).getPropertyValue(param.toLowerCase().replace('@', '--m-on-'));
             innerColor = val ? val : inner
          }
          this.color = innerColor;
