fe45a5b14edb449b824f36d81c1e68b04be32caf
leafweb <leafweb.io@gmail.com>
2024-11-20 09:56:12 +0330

    update

--- Marshmallow/components/m-linear-progress.js
+++ Marshmallow/components/m-linear-progress.js
@@ -2,13 +2,14 @@
 
 // <m-linear-progress> 
 export default class MLinearProgress extends HTMLElement {
-   #value;#min;#max;
+   #value;#min;#max;#sharp;
    constructor() {
       super();
       this.shadow = this.attachShadow({mode: 'open'});
       this.#value = undefined;
       this.#min = 0;
       this.#max = 100;
+      this.#sharp = false;
    }
    static get observedAttributes() {
       return ['value','min','max','sharp'];
@@ -18,13 +19,31 @@ export default class MLinearProgress extends HTMLElement {
          case 'value':
             this.#value = newValue;
             break;
+         case 'min':
+            this.#min = newValue;
+            break;
+         case 'max':
+            this.#max = newValue;
+            break;
+         case 'sharp':
+            this.#sharp = this.hasAttribute('sharp');
+            break;
       }
       this.#render();
    }
    connectedCallback() {
       this.#render();
    }
+   set value(x) {this.setAttribute('value', x)}
+   set min(x) {this.setAttribute('min', x)}
+   set max(x) {this.setAttribute('max', x)}
+   set sharp(x) {x?this.setAttribute('sharp',''):this.removeAttribute('sharp')}
+   get value() {this.getAttribute('value')}
+   get min() {this.getAttribute('min')}
+   get max() {this.getAttribute('max')}
+   get sharp() {this.hasAttribute('sharp')}
    #render() {
+      let sharp = this.#sharp;
       let clamp = (min, max, num) => Math.max(min,Math.min(max,num));
       let min = this.#min || 0;
       let max = this.#max || 100;
@@ -62,7 +81,7 @@ export default class MLinearProgress extends HTMLElement {
                x2="${1000}" 
                y2="${h/2}" 
                stroke-width="${h}"
-               stroke-linecap="round"/>
+               stroke-linecap="${sharp?'square':'round'}"/>
             <line 
                part="progress"
                x1="0" 
@@ -70,7 +89,7 @@ export default class MLinearProgress extends HTMLElement {
                x2="${this.#value ? progress*10 : 1000}" 
                y2="${h/2}"
                stroke-width="${(this.#value == 0? 0 : h)}"
-               stroke-linecap="round"/>
+               stroke-linecap="${sharp?'square':'round'}"/>
          </svg>
       `;}
    }
